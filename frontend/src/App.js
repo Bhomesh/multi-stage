cf2e9a8fbc90dac0a057684ab01328f5d3f2a3e8
Bhomesh <bhomeshrazdan@gmail.com>
2024-08-12 15:24:03 +0530

    inital commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,27 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            // const response = await fetch('http://localhost:5000/api/data'); // Replace with your backend URL
+            const response = await fetch('http://backend-service/api/data');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const jsonData = await response.json();
+            setData(jsonData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
   return (
     <div className="App">
       <header className="App-header">
